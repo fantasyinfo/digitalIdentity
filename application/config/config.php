a5ee4234237eccde5fe7afb7649017a9f2ec04a7
fantasyinfo <gs27349gs@gmail..com>
2023-01-04 11:15:47 -0800

    uppp

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Kolkata');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/scanStudentProject/';
+$config['base_url'] = 'http://localhost/scanStudentProject/digitalIdentity/';
 
 /*
 |--------------------------------------------------------------------------
